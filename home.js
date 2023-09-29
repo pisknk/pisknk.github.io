e9821f6c93305407f9b5c30a2892d6e46aab29cc
pisknk <2001101888@student.buksu.edu.ph>
2023-09-29 20:09:07 +0800

    Combo Release (v4)
    Stupid things I wrote. Including this website, now with major changes and lifetime improvements. Combining updates that haven’t been released (from 3.3 and 3.4)  
    
    - Added Landing Page
    - Added About Page (WIP, haven't linked yet)
    - You can now access the website without specifying the .html file - Reorganized Everything, from File management to reducing unecessary code.
    - Under the hood improvements
    
    Not just that, we also fixed: 
    - An Issue where website content doesn’t fit to user’s screen.
    - An Issue where background images also moves when scrolling on Safari Browser.
    - An Issue where website zoom is an issue to larger phones.
    
    Thanks for standing by! We will add more updates and features to the site. Current features/projects we pitched in:  
    
    - Secure Password Generator Homepage
    - What the Void?! - Price Calculator/iOS calculator clone
    - Knowledge Center
    - Homer’s Webpage  
    
    They are currently in the lowest priority now, but rest assured we will add them in a couple of days. Mobile Data is a scarce resource right now.
    
    Thanks for understanding!

--- home.js
+++ home.js
@@ -1,22 +1,61 @@
 document.addEventListener('DOMContentLoaded', function() {
     var titlebar = document.getElementById('titlebar');
     var initialOffset = titlebar.offsetTop;
-
+  
     window.addEventListener('scroll', function() {
-        var scrollPosition = window.scrollY;
-
-        if (scrollPosition > initialOffset + 20) {
-            titlebar.style.position = 'fixed';
-            titlebar.style.top = '20px';
-            titlebar.style.left = '50%';
-            titlebar.style.transform = 'translateX(-50%)';
-            titlebar.style.zIndex = '999'; // Set a higher z-index to keep it on top
-        } else {
-            titlebar.style.position = 'absolute';
-            titlebar.style.top = initialOffset + 'px';
-            titlebar.style.left = '50%';
-            titlebar.style.transform = 'translateX(-50%)';
-            titlebar.style.zIndex = '0'; // Reset the z-index
-        }
+      var scrollPosition = window.scrollY;
+  
+      if (scrollPosition > initialOffset + 20) {
+        titlebar.style.position = 'fixed';
+        titlebar.style.top = '20px';
+        titlebar.style.left = '50%';
+        titlebar.style.transform = 'translateX(-50%)';
+        titlebar.style.zIndex = '999'; // Set a higher z-index to keep it on top
+      } else {
+        titlebar.style.position = 'absolute';
+        titlebar.style.top = initialOffset + 'px';
+        titlebar.style.left = '50%';
+        titlebar.style.transform = 'translateX(-50%)';
+        titlebar.style.zIndex = '0'; // Reset the z-index
+      }
     });
-});
+  
+    // Function to show the popup and disable scrolling
+    function showPopup() {
+      var popup = document.getElementById('popup');
+      popup.style.display = 'block';
+  
+      // Disable scrolling
+      document.body.style.overflow = 'hidden';
+    }
+  
+    // Function to hide the popup and enable scrolling
+    function hidePopup() {
+      var popup = document.getElementById('popup');
+      popup.style.display = 'none';
+  
+      // Enable scrolling
+      document.body.style.overflow = 'auto';
+    }
+  
+    // Function to check screen width and show/hide the popup
+    function checkScreenWidth() {
+      var popup = document.getElementById('popup');
+      if (window.innerWidth >= 1024) {
+        showPopup();
+      } else {
+        hidePopup();
+      }
+    }
+  
+    // Initial check on page load
+    checkScreenWidth();
+  
+    // Listen for screen resize events
+    window.addEventListener('resize', checkScreenWidth);
+  
+    // Close button click event
+    var closeButton = document.getElementById('close-button');
+    closeButton.addEventListener('click', hidePopup);
+  });
+  
---
+++ poems/awrote/scripts.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Fetch the poem data from the JSON file
+    fetch('poems.json')
+        .then(response => response.json())
+        .then(data => {
+            // Find the poem with the title
+            const poem = data.find(poem => poem.title === "a");
+
+            // Check if the poem was found
+            if (poem) {
+                // Update the HTML elements with the poem data
+                document.querySelector('.mica-box h2').textContent = poem.title;
+                document.querySelector('.poem-text').innerHTML = poem.text;
+            } else {
+                console.error('Poem not found in JSON data.');
+            }
+        })
+        .catch(error => {
+            console.error('Error fetching poem data:', error);
+        });
+});

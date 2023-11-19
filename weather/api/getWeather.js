8db888608b6ca9659480369e5df28ecfda8d04e7
pisknk <nbayron29@gmail.com>
2023-11-19 22:23:34 +0800

    testing vercel (4.3i)

--- weather/api/getWeather.js
+++ weather/api/getWeather.js
@@ -1,24 +1,28 @@
-// api/getWeather.js
+// getWeather.js
 
 const fetch = require('node-fetch');
 
 module.exports = async (req, res) => {
+  const { city } = req.query; // Get the city from the query parameters
+
+  if (!city) {
+    return res.status(400).json({ error: 'City parameter is missing' });
+  }
+
+  const apiKey = process.env.OPENWEATHERMAP_API_KEY; // Retrieve API key from environment variables
+
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
   try {
-    const { city } = req.query;
-    const apiKey = process.env.OPENWEATHERMAP_API_KEY;
-
-    if (!city || !apiKey) {
-      return res.status(400).json({ error: 'City name or API key missing' });
-    }
-
-    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-
     const response = await fetch(apiUrl);
-    const weatherData = await response.json();
-
-    return res.status(200).json(weatherData);
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    const data = await response.json();
+    
+    // Process the weather data and send the response to the client
+    res.status(200).json(data);
   } catch (error) {
-    console.error('Error fetching weather data:', error);
-    return res.status(500).json({ error: 'Failed to fetch weather data' });
+    res.status(500).json({ error: 'Failed to fetch weather data' });
   }
 };
